9154ee9118654751f3ad238b9a7809324e95131a
Leonardo Pavan <leonardomattos@professor.educacao.sp.gov.br>
2024-05-15 13:02:12 -0300

    Create bolsa_de_estudos.py

---
+++ bolsa_de_estudos.py
@@ -0,0 +1,7 @@
+gpa = 3.6
+top_10 = False
+voluntario_horas = 105
+if gpa >= 3.5 or top_10 or voluntario_horas > 100:
+    print("Você é elegível para a bolsa de estudos.")
+else:
+    print("Você não é elegível para a bolsa de estudos.")
